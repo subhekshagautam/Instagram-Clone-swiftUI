3e07290c8343bc3e5e3e78e10835e1141460b2a5
Subheksha Gautam <subhekshagautam@gmail.com>
2022-08-03 12:14:56 +1000

    Initial Commit

---
+++ Instagram_Clone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instagram_Clone
+//
+//  Created by Subheksha Gautam on 3/8/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Instagram_Clone/Instagram_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_CloneApp.swift
+//  Instagram_Clone
+//
+//  Created by Subheksha Gautam on 3/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
