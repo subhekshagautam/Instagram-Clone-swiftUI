888e5958e94f71ec32fab47f70640a9e1aa56f5f
Subheksha Gautam <subhekshagautam@gmail.com>
2022-08-09 23:06:33 +1000

    changes

---
+++ Instagram_Clone/Views/Notifications/NotificationsView.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationsView.swift
+//  Instagram_Clone
+//
+//  Created by Subheksha Gautam on 3/8/2022.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    
+    var body: some View {
+        
+        Text("Notification view")
+        
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
